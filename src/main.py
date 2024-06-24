e79f1305540d5f8eebd4dbc6196ff024e1e2594e
caiowirthmann <caiowirthmann@gmail.com>
2024-06-23 23:05:02 -0300

    update

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
 #this is the main code for the project
+from textnode import TextNode
+
 print('test for project')
+
+def main():
+    class1 = TextNode("texto de teste", "bold")
+    class2 = TextNode("texto body", "block", "https://bootdev.com")
+
+    print(class1.__repr__())
+    print(class2.__repr__())
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,19 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def __eq__(self, other) -> bool:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+teste_classe = TextNode("texto de exemplo", "texto é um header")
+teste_classe2 = TextNode("texto de exemplo", "texto é um header")
+
+# print(teste_classe.__repr__())
+# print(teste_classe2.__repr__())
+
+# print (teste_classe.__eq__(teste_classe2))
