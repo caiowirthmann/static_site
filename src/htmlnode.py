536dae5f2fe5f4ae024faf27b7ddd0dbe852068a
Caio Wirthmann <108591699+caiowirthmann@users.noreply.github.com>
2024-07-03 16:37:05 -0300

    up

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,9 @@
 class HTMLnode:
     def __init__(self, tag = None, value = None, children = None, props = None):        
         pass
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        
