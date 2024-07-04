aab68e7efad8c7f61f761fe6d724098f857e906a
caiowirthmann <caiowirthmann@gmail.com>
2024-07-04 00:12:45 -0300

    htmlnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,9 +1,20 @@
 class HTMLnode:
     def __init__(self, tag = None, value = None, children = None, props = None):        
-        pass
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props #    props is a dict{} with attributes of html tag
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError ("to html method not implemented")
 
     def props_to_html(self):
-        
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+          props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
