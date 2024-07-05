370fda1aec3dc95244b23489f30a15501a4d3555
Caio Wirthmann <108591699+caiowirthmann@users.noreply.github.com>
2024-07-05 13:03:50 -0300

    update

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,26 @@ class HTMLnode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLnode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError ("leaf nodes require a value to render as a HTML")
+        
+        if self.tag is None:
+            return f"{self.value}"
+        
+        #starts constructing the HTML string <....
+        html_string = f"<{self.tag}"
+
+        if self.props:
+            for key, val in self.props.items():
+                # add HTML tag and corresponding value. example--> "href" tag and link
+                html_string += f' {key}="{val}"'
+        #close first tag <.., and value from node and close tag with </tag>
+        html_string += f'>{self.value}</{self.tag}>'
+
+        return html_string
